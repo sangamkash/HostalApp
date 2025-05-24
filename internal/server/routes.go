904630109aa18ded18ee5096a2e7f213ea5fbdc3
Sangamkash <sangamkash@gmail.com>
2025-05-24 12:34:57 +0530

    added all file

---
+++ internal/server/routes.go
@@ -0,0 +1,72 @@
+package server
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+
+	"github.com/gofiber/contrib/websocket"
+)
+
+func (s *FiberServer) RegisterFiberRoutes() {
+	// Apply CORS middleware
+	s.App.Use(cors.New(cors.Config{
+		AllowOrigins:     "*",
+		AllowMethods:     "GET,POST,PUT,DELETE,OPTIONS,PATCH",
+		AllowHeaders:     "Accept,Authorization,Content-Type",
+		AllowCredentials: false, // credentials require explicit origins
+		MaxAge:           300,
+	}))
+
+	s.App.Get("/", s.HelloWorldHandler)
+
+	s.App.Get("/health", s.healthHandler)
+
+	s.App.Get("/websocket", websocket.New(s.websocketHandler))
+
+}
+
+func (s *FiberServer) HelloWorldHandler(c *fiber.Ctx) error {
+	resp := fiber.Map{
+		"message": "Hello World",
+	}
+
+	return c.JSON(resp)
+}
+
+func (s *FiberServer) healthHandler(c *fiber.Ctx) error {
+	return c.JSON(s.db.Health())
+}
+
+func (s *FiberServer) websocketHandler(con *websocket.Conn) {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	go func() {
+		for {
+			_, _, err := con.ReadMessage()
+			if err != nil {
+				cancel()
+				log.Println("Receiver Closing", err)
+				break
+			}
+		}
+	}()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			payload := fmt.Sprintf("server timestamp: %d", time.Now().UnixNano())
+			if err := con.WriteMessage(websocket.TextMessage, []byte(payload)); err != nil {
+				log.Printf("could not write to socket: %v", err)
+				return
+			}
+			time.Sleep(time.Second * 2)
+		}
+	}
+}
